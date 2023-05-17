e6f40bbea4834a991478e65a5176db8988212992
Muhsin Bashir <101365717+MuhsinBashirrr@users.noreply.github.com>
2023-05-17 22:31:42 +0530

    Create AnimalQuizGame.py

---
+++ AnimalQuizGame.py
@@ -0,0 +1,17 @@
+def check_guess(guess, answer):
+    global score
+    still_guessing = True
+    attempt = 0
+    while still_guessing and attempt < 3:
+        if guess.lower() == answer.lower():
+            print("Correct Answer")
+            score = score + 1
+            still_guessing = False
+        else:
+            if attempt < 2:
+                guess = input("Sorry Wrong Answer, try again")
+            attempt = attempt + 1
+    if attempt == 3:
+        print("The Correct answer is ",answer )
+    
+score = 0
