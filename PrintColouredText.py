b55bbbe1c636504d547319ded758967617f2ca9a
Muhsin Bashir <101365717+MuhsinBashirrr@users.noreply.github.com>
2023-05-20 23:36:48 +0530

    Create PrintColouredText.py

---
+++ PrintColouredText.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Muhsin Bashir "+ Fore.YELLOW+ Back.BLUE+"I love Machine Learning")
+print(Back.CYAN+"Hi My name is Muhsin Bashir")
+print(Fore.RED + Back.GREEN+ "Hi My name is Muhsin Bashir")
