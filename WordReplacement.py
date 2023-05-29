16fe8a455fad3f2ff2c943b1b70efd0f47714335
Muhsin Bashir <101365717+MuhsinBashirrr@users.noreply.github.com>
2023-05-29 22:11:27 +0530

    Add files via upload

---
+++ WordReplacement.py
@@ -0,0 +1,11 @@
+def replace_word():
+
+    str = "Hy mUHSIN"
+
+    word_to_replace = input("enter the word you want to replace: ")
+
+    word_replacement = input("enter the word you want to replace with: ")
+
+    print(str.replace(word_to_replace,word_replacement ))
+
+replace_word()
